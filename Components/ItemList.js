fa0d1fc084dadb1312e4ff57ccd2adf7e4477bdb
Joseph Asante <freecoding1@gmail.com>
2020-04-30 11:20:05 +0000

    redered first item list

--- Components/ItemList.js
+++ Components/ItemList.js
@@ -5,21 +5,21 @@ import Item from './Item'
 
 export class ItemList extends Component {
   list = [
-    { id: "1", name: "Bike Jacket SoftShel Warm", price: "$99", image: require('../assets/hoodie2.jpg') },
-    { id: "2", name: "I lOVE CODING", price: "$100", image: require('../assets/hoodie3.webp') },
-    { id: "3", name: "CODETRAIN", price: "$200", image: require('../assets/hoodie2.jpg') },
-    { id: "4", name: "React Native", price: "$90", image: require('../assets/hoodie4.jpg') },
+    { id: "1", name: "Bike Jacket SoftShel Warm", price: "$99", image: require('../assets/jacket.jpg') },
+    { id: "2", name: "I lOVE CODING", price: "$100", image: require('../assets/jacket3.jpg') },
+    { id: "3", name: "CODETRAIN", price: "$200", image: require('../assets/jacket1.jpg') },
+    { id: "4", name: "React Native", price: "$90", image: require('../assets/jacket4.jpeg') },
 
   ]
   render() {
     return (
       <ScrollView  style={styles.container} showsVerticalScrollIndicator={false}>
          <View style={styles.iconContainer}>
-          <TouchableOpacity style={styles.leftIcon}>
+          <TouchableOpacity style={styles.firstIcon}>
             <AntDesign name="arrowleft" size={20}  />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.rightIcon}>
-            <Feather name="shopping-cart" size={20}  />
+          <TouchableOpacity style={styles.secondIcon}>
+            <Feather name="shopping-cart" size={20} />
           </TouchableOpacity>
         </View>
       
@@ -51,65 +51,69 @@ export class ItemList extends Component {
 
 const styles = StyleSheet.create({
   container: {
-  
-    marginHorizontal: 10,
-    backgroundColor: "#ededed",
+    marginVertical: -80,
+    backgroundColor: "#ddf"
   },
+
+  firstIcon: {
+    backgroundColor: "white",
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  secondIcon: {
+    position: "absolute",
+    right: 0,
+    backgroundColor: "white",
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+    color: "blue",
+    marginLeft: 15,
+  },
+  
   filter:{
-    position:"absolute",
+    position: "absolute",
     top:10,
     right:50,
-    fontSize:20,
-    color:"blue",
-    fontWeight:"bold"
+    fontSize:15,
+    color: "blue",
+    
     
   },
   input:{
-    borderWidth:0.5,
-    height:50,
-    marginHorizontal:30,
-    borderRadius:15,
-    backgroundColor:"white",
-    paddingLeft:20
+    backgroundColor: "#ffff",
+    height:45,
+    marginHorizontal:25,
+    borderRadius:10,
+    paddingLeft:20,
   },
   inputcontainer:{
     marginBottom:20
   },
   iconContainer: {
     flexDirection: "row",
-    marginVertical:5,
-    marginHorizontal:15
+    marginVertical: 5,
+    marginHorizontal: 15
   },
-  leftIcon: {
-    backgroundColor:"white",
-    borderRadius:50,
-    width:40,
-    height:40,
-    justifyContent:"center",
-    alignSelf:"center",
-    alignItems:"center",
-  },
-  rightIcon: {
-    position: "absolute",
-    right: 0,
-    backgroundColor:"white",
-    borderRadius:50,
-    width:40,
-    height:40,
-    justifyContent:"center",
-    alignSelf:"center",
-    alignItems:"center",
 
-  },
   textContainer: {
     marginBottom: 20,
-    marginLeft: 20
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical:10
-  },
+    marginLeft: 20,
+  }
+  
 })
 
 export default ItemList
