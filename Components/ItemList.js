bd8d2e542a24b8ca4f50f3b34026b67a9c477dbc
Joseph Asante <freecoding1@gmail.com>
2020-05-01 09:35:11 +0000

    ItemList redered successfully

--- Components/ItemList.js
+++ Components/ItemList.js
@@ -5,10 +5,10 @@ import Item from './Item'
 
 export class ItemList extends Component {
   list = [
-    { id: "1", name: "Bike Jacket SoftShel Warm", price: "$99", image: require('../assets/hoodie2.jpg') },
-    { id: "2", name: "I lOVE CODING", price: "$100", image: require('../assets/hoodie3.webp') },
-    { id: "3", name: "CODETRAIN", price: "$200", image: require('../assets/hoodie2.jpg') },
-    { id: "4", name: "React Native", price: "$90", image: require('../assets/hoodie4.jpg') },
+    { id: "10", name: "Woman Jacket Yellow Color", price: "$70", image: require('../assets/jacket.jpg') },
+    { id: "20", name: "Wonder Wonan Jacket", price: "$200", image: require('../assets/jacket3.jpg') },
+    { id: "30", name: "Woman Jacket B&W", price: "$50", image: require('../assets/jacket1.jpg') },
+    { id: "40", name: "Simple Woman Jacket", price: "$45", image: require('../assets/jacket4.jpeg') },
 
   ]
   render() {
@@ -24,12 +24,12 @@ export class ItemList extends Component {
         </View>
       
         <View style={styles.textContainer}>
-          <Text style={styles.text}>Woman Jacket</Text>
+          <Text style={styles.headerText}>Woman Jacket</Text>
         </View>
 
           <View style={styles.inputcontainer}>
-            <TextInput placeholder="Search Jacket"  placeholderTextColor="#858383" style={styles.input} />
-            <Text style={styles.filter}>Filter</Text>
+            <TextInput placeholder="Search Jacket"  placeholderTextColor="#858383" style={styles.searchInput} />
+            <Text style={styles.filterText}>Filter</Text>
           </View>
 
         <View>
@@ -76,23 +76,35 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 
-  filter:{
-    position:"absolute",
+  textContainer: {
+    marginBottom: 20,
+    marginLeft: 20,
+  },
+
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+    color: "blue",
+    marginLeft: 15,
+  },
+
+  searchInput:{
+    backgroundColor: "#ffff",
+    height:45,
+    marginHorizontal:25,
+    borderRadius:10,
+    paddingLeft:20,
+  },
+
+  filterText:{
+    position: "absolute",
     top:10,
     right:50,
-    fontSize:20,
-    color:"blue",
-    fontWeight:"bold"
-    
+    fontSize:15,
+    color: "blue",
   },
-  input:{
-    borderWidth:0.5,
-    height:50,
-    marginHorizontal:30,
-    borderRadius:15,
-    backgroundColor:"white",
-    paddingLeft:20
-  },
+  
   inputcontainer:{
     marginBottom:20
   },
@@ -102,15 +114,7 @@ const styles = StyleSheet.create({
     marginHorizontal:15
   },
 
-  textContainer: {
-    marginBottom: 20,
-    marginLeft: 20
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical:10
-  },
+  
 })
 
 export default ItemList
